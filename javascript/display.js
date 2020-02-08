48409148ef62008776d265a9ed5beba89a5b7569
samFredLumley <samfredlumley@gmail.com>
2020-02-08 03:04:42 -0500

    added node

--- javascript/display.js
+++ javascript/display.js
@@ -1,38 +1,38 @@
 // This script converts the json transcript file to html
 // so that hyperaudio-lite can interpret it
 
-<script>
-function handleFileSelect(evt) {
-    var files = evt.target.files; // FileList object
 
-    // Loop through the FileList and render image files as thumbnails.
-    for (var i = 0, f; f = files[i]; i++) {
+// function handleFileSelect(evt) {
+//     var files = evt.target.files; // FileList object
 
-    // Only process image files.
-    if (!f.type.match('image.*')) {
-        continue;
-    }
+//     // Loop through the FileList and render image files as thumbnails.
+//     for (var i = 0, f; f = files[i]; i++) {
 
-    var reader = new FileReader();
+//     // Only process image files.
+//     if (!f.type.match('image.*')) {
+//         continue;
+//     }
 
-    // Closure to capture the file information.
-    reader.onload = (function(theFile) {
-        return function(e) {
-        // Render thumbnail.
-        var span = document.createElement('span');
-        span.innerHTML = ['<img class="thumb" src="', e.target.result,
-                            '" title="', escape(theFile.name), '"/>'].join('');
-        document.getElementById('list').insertBefore(span, null);
-        };
-    })(f);
+//     var reader = new FileReader();
 
-    // Read in the image file as a data URL.
-    reader.readAsDataURL(f);
-    }
-}
+//     // Closure to capture the file information.
+//     reader.onload = (function(theFile) {
+//         return function(e) {
+//         // Render thumbnail.
+//         var span = document.createElement('span');
+//         span.innerHTML = ['<img class="thumb" src="', e.target.result,
+//                             '" title="', escape(theFile.name), '"/>'].join('');
+//         document.getElementById('list').insertBefore(span, null);
+//         };
+//     })(f);
 
-document.getElementById('files').addEventListener('change', handleFileSelect, false);
-</script>
+//     // Read in the image file as a data URL.
+//     reader.readAsDataURL(f);
+//     }
+// }
+
+// document.getElementById('files').addEventListener('change', handleFileSelect, false);
+
 
 // TODO: I need to change this to get the structure for hyper
 // This is: 
