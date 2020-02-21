6c3f449ce59f004f48876d3c0d97f3d12b213aa1
samFredLumley <samfredlumley@gmail.com>
2020-02-21 14:56:53 -0500

    trying to add python

--- index-express.js
+++ index-express.js
@@ -19,14 +19,35 @@ const fs = require('fs');
 app.use('/jquery', express.static(__dirname + '/node_modules/jquery/dist/'));
 
 
-let {PythonShell} = require('python-shell');
+// trying to get python to work
 
-PythonShell.runString('x=1+1;print(x)', null, function (err) {
+const {PythonShell} = require('python-shell');
+
+PythonShell.runString('print("hi")', null, function (err) {
     if (err) throw err;
     console.log('finished');
     });
 
 console.log('hey');
+
+// the below works!
+// const {spawn} = require('child_process');
+// app.get('/py', (req, res) => {
+//     var dataToSend;
+//     // spawn new child process to call the python script
+//     const python = spawn('python', ['script.py']);
+//     // collect data from script
+//     python.stdout.on('data', function (data) {
+//     console.log('Pipe data from python script ...');
+//     dataToSend = data.toString();
+//     });
+//     // in close event we are sure that stream from child process is closed
+//     python.on('close', (code) => {
+//     console.log(`child process close all stdio with code ${code}`);
+//     // send data to browser
+//     res.send(dataToSend)
+//     });
+// })
 
 // var sassMiddleware = require('node-sass-middleware');
 
